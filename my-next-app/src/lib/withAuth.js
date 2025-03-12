d35ad05bd8bbe9401eaff5b1b1e6994bde3f864a
KumarRoushan9234 <roushan9234620012@gmail.com>
2025-03-12 15:14:42 +0530

    new

--- my-next-app/src/lib/withAuth.js
+++ my-next-app/src/lib/withAuth.js
@@ -1,24 +1,15 @@
-import { getSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
 export default function withAuth(Component) {
   return function ProtectedRoute(props) {
-    const [loading, setLoading] = useState(true);
+    const { data: session } = useSession();
     const router = useRouter();
 
-    useEffect(() => {
-      async function checkAuth() {
-        const session = await getSession();
-        if (!session) {
-          router.replace("/login");
-        }
-        setLoading(false);
-      }
-      checkAuth();
-    }, []);
-
-    if (loading) return <p className="text-center text-gray-500">Loading...</p>;
+    if (!session) {
+      router.push("/login");
+      return null;
+    }
 
     return <Component {...props} />;
   };
